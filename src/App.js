18ceb76776015245903a50f12a60b5757614ac66
mila-developer <camilalyra.abreu@gmail.com>
2023-01-05 20:35:21 +0000

    add: background and image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import img1 from './img1.png';
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container sx={{backgroundColor: 'pink', height:'100vh', pl:3}}  component='main'>
+      <Grid xs={12} component='section' sx={{
+          backgroundColor: '#282C4B',
+          height:'40vh'
+        }}>
+
+        <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+          <img src={img1} alt=""/>
+          <Box>
+          <p style={{color: '#F17216'}}>WHY CHOOSE US</p>
+          <p style={{color: '#fff'}}>Take on any Challenge of the Digital World</p>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
